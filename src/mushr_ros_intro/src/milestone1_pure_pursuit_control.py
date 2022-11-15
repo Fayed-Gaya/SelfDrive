30d9330d1a0524db3986b0b1229bb4897aba181c
Hanwen Zhao <gayafayed@gmail.com>
2022-11-15 08:35:04 -0500

    Complete Pure Pursuit control program

--- src/mushr_ros_intro/src/milestone1_pure_pursuit_control.py
+++ src/mushr_ros_intro/src/milestone1_pure_pursuit_control.py
@@ -32,7 +32,7 @@ LIDAR_RIGHT = 270
 LIDAR_RANGE = 30
 
 # Set the look ahead distance
-LOOK_AHEAD_DISTANCE = 0.5
+LOOK_AHEAD_DISTANCE = 0.2
 
 # Set the waypoint coordinates input file
 REFERENCE_PATH_INPUT_FILE = "/home/hanwen/catkin_ws/src/mushr_ros_intro/src/reference_path_ideal.txt"
@@ -69,22 +69,22 @@ def nearest_waypoint_current_pose(reference_path, current_pose):
     # current_pose_array = np.expand_dims(current_pose_array, axis=1).T
 
 
-    print(np.shape(current_pose))
-    print(np.shape(reference_path))
+    # print(np.shape(current_pose))
+    # print(np.shape(reference_path))
 
     # Calculate an array of all of the distances between the current pose and the poses of the waypoints that make up the reference path
     distance_array = np.linalg.norm(current_pose - reference_path, axis=1)
 
-    print('DIST ARRAY %s' % np.shape(distance_array))
+    # print('DIST ARRAY %s' % np.shape(distance_array))
     # Calculate the minimim distance way point as an array
     min_val_index = np.argmin(distance_array)
-    print('MIN INDEX %s' % min_val_index)
-    print('MIN VALUE %s' % distance_array[min_val_index])
+    # print('MIN INDEX %s' % min_val_index)
+    # print('MIN VALUE %s' % distance_array[min_val_index])
     # Unpack the x and y coordinates of the nearest waypoint
     nearest_x_coordinate = reference_path[min_val_index][0]
     nearest_y_coordinate = reference_path[min_val_index][1]
 
-    return np.array(nearest_x_coordinate, nearest_y_coordinate)
+    return np.array([nearest_x_coordinate, nearest_y_coordinate])
 
 def look_ahead_pt_calculator(current_pose_orientation, current_pos, l_d):
     """
@@ -99,11 +99,40 @@ def look_ahead_pt_calculator(current_pose_orientation, current_pos, l_d):
     yaw = r.as_euler('zyx', degrees=False)[0]
 
     look_ahead_x = current_pos[0] + l_d * np.cos(yaw)
-    look_ahead_y = current_pos[1] + l_d * np.cos(yaw)
+    look_ahead_y = current_pos[1] + l_d * np.sin(yaw)
 
     look_ahead_pt = np.array([look_ahead_x, look_ahead_y])
 
     return look_ahead_pt
+
+def nearest_waypoint_lookahead_point(reference_path, lookahead_point):
+    """
+    Calculate the nearest waypoint to the look ahead point along the reference path.
+    """
+
+    # Calculate an array of all of the distances between the current pose and the poses of the waypoints that make up the reference path
+    distance_array = np.linalg.norm(lookahead_point - reference_path, axis=1)
+
+    # print('DIST ARRAY %s' % np.shape(distance_array))
+    # Calculate the minimim distance way point as an array
+    min_val_index = np.argmin(distance_array)
+    # print('MIN INDEX %s' % min_val_index)
+    # print('MIN VALUE %s' % distance_array[min_val_index])
+    # Unpack the x and y coordinates of the nearest waypoint
+    nearest_x_coordinate = reference_path[min_val_index][0]
+    nearest_y_coordinate = reference_path[min_val_index][1]
+
+    return np.array([nearest_x_coordinate, nearest_y_coordinate])
+
+def calculate_heading_error(current_pos, nn1, look_ahead_point, nn2):
+    # Calculate vector 1
+    v1 = (look_ahead_point - current_pos) / (np.linalg.norm(current_pos - look_ahead_point) + 1e-6)
+    # Calcuate vector 2
+    v2 = (nn2 - nn1) / (np.linalg.norm(nn1 - nn2) + 1e-6)
+    # Calculate heading error
+    heading_error = np.arctan2(np.dot(np.cross(v1, v2), np.array([0,0,1])),  np.dot(v1, v2))
+
+    return heading_error
 
 def current_pose_callback(data, args):
     """
@@ -125,17 +154,41 @@ def current_pose_callback(data, args):
 
     # Calculate the nearest waypoint tp the current position
     np_actual = nearest_waypoint_current_pose(reference_path, current_pose)
-
     # Calculate the lookahead point
     look_ahead_pt = look_ahead_pt_calculator(data.pose.orientation, current_pose, LOOK_AHEAD_DISTANCE)
 
-    print(look_ahead_pt)
-
-    # Calculate the nearest waypoint to the the 
-
+    # Calculate the nearest waypoint to the the lookahead point
+    lh_np = nearest_waypoint_lookahead_point(reference_path, look_ahead_pt)
+    
+    # print(look_ahead_pt)
+    # print(lh_np)
+    # print('\n')
     # Calculate the L2 Norm, cross-track error
     cross_track_error = np.linalg.norm(np_actual - current_pose)
+    # Calculate the heading error
+    heading_error = calculate_heading_error(current_pose, np_actual, look_ahead_pt, lh_np)
+    heading_error = - heading_error[2]
+    # print(cross_track_error)
+
+    # Caculate the steering angle using the PID
+    # steering_angle = pid(control_value + cross_track_error)
+    VEL = 2.0
+    # Pure Pursuit
+    L = 0.5
+    pursit_steering_angle = -np.arctan(2*L*np.sin(heading_error)/(LOOK_AHEAD_DISTANCE))
+    # print('Pure Pursuit %s' % pursit_steering_angle)
+
+    # Stanely
+    K = 1.0
+    if heading_error < 0: cross_track_error = -cross_track_error
+    stanely_steering_angle = np.arctan(K*cross_track_error/(VEL+1e-6)) + (100 * - heading_error)
+    print('Stanely %s' % stanely_steering_angle)
     
+
+    # Create the drive message containing the corrective steering angle and speed
+    drive = AckermannDrive(steering_angle=stanely_steering_angle, speed=VEL)
+    # Publish the steering angle and speed
+    pub_controls.publish(AckermannDriveStamped(drive=drive))
 
 def send_init_pose(pub_init_pose, init_pose):
     """
@@ -164,9 +217,6 @@ def lidar_callback(data, args):
     :param data: The data type of the publishing node
     :param args: The node that publishes
     """
-
-    
-
     
     # Calculate the distance from the car to the wall at its left
     left_dist = np.nanmean(np.array(data.ranges[LIDAR_LEFT-LIDAR_RANGE:LIDAR_LEFT+LIDAR_RANGE]))
@@ -193,12 +243,8 @@ if __name__ == "__main__":
     # Load the reference path
     reference_path = reference_path_loader()
 
-
-
     # Initialize the baseline controller node
     rospy.init_node('baseline_pid')
-
-    
 
     # Collect the control topic to publish the steering instructions to
     control_topic = rospy.get_param("~control_topic", "/car/mux/ackermann_cmd_mux/input/navigation")
@@ -208,19 +254,23 @@ if __name__ == "__main__":
     # Collect the intial pose topic
     init_pose_topic = rospy.get_param("~init_pose_topic", "/initialpose")
     # Publish the inital pose
-    pub_init_pose = rospy.Publisher(init_pose_topic, PoseWithCovarianceStamped, queue_size=1)
+    pub_init_pose = rospy.Publisher(init_pose_topic, PoseWithCovarianceStamped, queue_size=5)
 
     # initialize the car's initial pose
     init_pose = [0., 0., np.pi]
     send_init_pose(pub_init_pose, init_pose)
+
+    # Set the control publish frequency
+    rospy.sleep(1.0)
 
     # Begin the LIDAR scan and subsequently trigger the steering and drive controls
     # sub_lidar_scan = rospy.Subscriber("/car/scan", LaserScan, lidar_callback, (pub_controls, reference_path))
 
     # Begin Pure Pursuit
     rospy.Subscriber('/car/car_pose', PoseStamped, current_pose_callback, (pub_controls, reference_path))
-    # Set the control publish frequency
+
     rospy.sleep(1.0)
+    
     
     # use rospy.spin and let lidar callback handle the control
     rospy.spin()
